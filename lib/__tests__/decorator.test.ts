37dfa4957dfe2b074bc0df8e256ebc87514e2ac7
Shashi Shekhar <shshekhar93@gmail.com>
2023-06-11 00:24:57 +0530

    Add decorator tests

---
+++ lib/__tests__/decorator.test.ts
@@ -0,0 +1,42 @@
+import { decorate } from "../decorator";
+import { HTTPRequest, HTTPResponse } from "../server-types";
+
+function searchParamsToObj(params?: URLSearchParams) {
+  return Object.fromEntries([
+    ...params?.entries() ?? []
+  ]);
+}
+
+describe('Decorator tests', () => {
+  it('Should parse url', () => {
+    const req = ({
+      url: '/foo/bar?cow=moo&dog=bark',
+      headers: {},
+    } as HTTPRequest);
+    const res = ({} as HTTPResponse);
+    decorate(req, res);
+    expect(req.parsedUrl?.pathname).toBe('/foo/bar');
+    expect(searchParamsToObj(req.parsedUrl?.searchParams)).toEqual({
+      cow: 'moo',
+      dog: 'bark',
+    });
+  });
+
+  it('Should skip parsing if url not defined', () => {
+    const req = ({} as HTTPRequest);
+    const res = ({} as HTTPResponse);
+    decorate(req, res);
+    expect(req.parsedUrl).toBe(undefined);
+  });
+
+  it('Should skip parsing if the url is already parsed', () => {
+    const empty = {};
+    const req = ({
+      url: '/foo/bar?cow=moo&dog=bark',
+      parsedUrl: empty,
+    } as HTTPRequest);
+    const res = ({} as HTTPResponse);
+    decorate(req, res);
+    expect(req.parsedUrl).toBe(empty)
+  });
+});
