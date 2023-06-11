7122706f445f273c3f8a7a3627354200c43ca8f9
Shashi Shekhar <shshekhar93@gmail.com>
2023-06-11 20:52:38 +0530

    Suppressing lint error for now

--- lib/types.ts
+++ lib/types.ts
@@ -4,6 +4,8 @@ import { HTTPResponse } from './http-response';
 export type Middleware = (req: HTTPRequest, res: HTTPResponse) => Promise<void>
 export type ErrorMiddleware = (err: Error, req: HTTPRequest, res: HTTPResponse) => Promise<void>;
 
+
+/*eslint-disable-next-line @typescript-eslint/ban-types */
 export declare class Route<T extends Function = Middleware> {
   path: RegExp | null;
   middlewares: Array<T>
